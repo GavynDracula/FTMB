d8c1a6e20c6aada672315e501c06b3a041bc79ac
Dracula <dracula.register@gmail.com>
2017-12-05 18:28:24 +0800

    InputLogger save state from Master to StableStorage

--- Master/packet_counter.c
+++ Master/packet_counter.c
@@ -10,7 +10,7 @@
 void* packet_counter(void* argv) {
     char err_buf[PCAP_ERRBUF_SIZE];
     pcap_t *src_nic;
-    pcap_t *dst_nic;
+    // pcap_t *dst_nic;
     pcap_loop_arg loop_arg;
     // struct bpf_program filter;
     // char filter_str[36];
@@ -22,21 +22,26 @@ void* packet_counter(void* argv) {
     pthread_mutex_unlock(pc_arg->mutex);
 
     src_nic = pcap_open_live(SRC_NIC, PACKET_MAX_SIZE, PROMISC_TRIGGER, TO_MS, err_buf);
-    dst_nic = pcap_open_live(DST_NIC, PACKET_MAX_SIZE, PROMISC_TRIGGER, TO_MS, err_buf);
+    // dst_nic = pcap_open_live(DST_NIC, PACKET_MAX_SIZE, PROMISC_TRIGGER, TO_MS, err_buf);
 
     /* Open src_nic for receiving packet and open dst_nic for sending packet */
-    if (src_nic == NULL || dst_nic == NULL) {
+    // if (src_nic == NULL || dst_nic == NULL) {
+        // fprintf(stderr, "Error: Packet Counter: pcap_open_live(): %s\n", err_buf);
+        // exit(2);
+    // }
+    // else
+        // fprintf(stdout,
+                // "Packet Counter: Open src_nic %s and dst_nic %s successfully!\n",
+                // SRC_NIC, DST_NIC);
+    if (src_nic == NULL) {
         fprintf(stderr, "Error: Packet Counter: pcap_open_live(): %s\n", err_buf);
         exit(2);
     }
     else
-        fprintf(stdout,
-                "Packet Counter: Open src_nic %s and dst_nic %s successfully!\n",
-                SRC_NIC, DST_NIC);
+        fprintf(stdout, "Packet Counter: Open src_nic %s successfully!\n", SRC_NIC);
 
     /* Filter packets(don't counter packets used for communication among FTMB) */
     // sprintf(filter_str, "icmp or tcp port %d", PRIVATE_PORT);
-    // sprintf(filter_str, "not tcp port %d", PRIVATE_PORT);
     // if (pcap_compile(src_nic, &filter, filter_str, OPTIMIZE_TRIGGER, 0) != 0) {
        // fprintf(stderr, "Error: Packet Counter: pcap_compile(): complie filter error\n");
        // exit(3);
@@ -47,18 +52,18 @@ void* packet_counter(void* argv) {
     // }
 
     loop_arg.pc_arg = pc_arg;
-    loop_arg.dst_nic = dst_nic;
+    // loop_arg.dst_nic = dst_nic;
     pcap_loop(src_nic, PACKET_NUM, get_packet, (u_char*)&loop_arg);
 
     pcap_close(src_nic);
-    pcap_close(dst_nic);
+    // pcap_close(dst_nic);
 
     return NULL;
 }
 
 void get_packet(u_char* arg, const struct pcap_pkthdr* pkthdr, const u_char* packet) {
     packet_counter_arg* pc_arg = ((pcap_loop_arg*)arg)->pc_arg;
-    pcap_t* dst_nic = ((pcap_loop_arg*)arg)->dst_nic;
+    // pcap_t* dst_nic = ((pcap_loop_arg*)arg)->dst_nic;
 
     struct ether_header* eth_header;
 
@@ -95,12 +100,22 @@ void get_packet(u_char* arg, const struct pcap_pkthdr* pkthdr, const u_char* pac
             }
         }
         else if (protocol == IPPROTO_ICMP) {
-            /* Just log icmp packet(ping) now */
-            pthread_mutex_lock(pc_arg->mutex);
-            *(pc_arg->counter) += 1;
-            pthread_mutex_unlock(pc_arg->mutex);
+            /* Just log icmp_echo packet(ping) now */
+            const u_char* icmp_header;
+            unsigned int ip_header_length;
             
-            fprintf(stdout, "Debug: packet counter receive 1 icmp packet\n");
+            ip_header_length = (*ip_header) & 0x0F;
+            ip_header_length = ip_header_length * 4;
+            icmp_header = ip_header + ip_header_length;
+
+            if (*icmp_header == 0) {
+                /* ICMP_ECHO */
+                pthread_mutex_lock(pc_arg->mutex);
+                *(pc_arg->counter) += 1;
+                pthread_mutex_unlock(pc_arg->mutex);
+            }
+            
+            // fprintf(stdout, "Debug: packet counter receive 1 icmp packet\n");
         }
         else
             return;
